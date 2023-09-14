3245a37fb193e3e672f1f40c8cce153bbc095ab0
dharamveer.dhiman@asb.co.nz <dharamveer.dhiman@asb.co.nz>
2023-09-14 17:27:18 +1200

    Add Common Schemas

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIxOTA3OQ/spectral.js?branch_slug=master&token=7e5cb56f-5404-4603-8a2b-07af09be48ca";
+export default { extends: ruleset };
